13fc38986a7a389f5015040ac4b49b7fcf192ab5
Phil Sturgeon <phil.sturgeon@wework.com>
2016-09-19 22:22:16 +0100

    This is why I normally write tests

--- src/Info.php
+++ src/Info.php
@@ -30,15 +30,27 @@ class Info
      */
     private function normalizeSemVerSuffix(string $version): string
     {
-        if (strpos($version, '-') === true) {
-            return $version;
+        foreach (['alpha', 'beta', 'RC'] as $suffix) {
+            if (stripos($version, $suffix) !== false) {
+                return str_replace($suffix, "-{$suffix}", $version);
+            }
         }
-        return str_ireplace(['alpha', 'beta', 'rc'], ['-alpha', '-beta', '-rc'], $version);
+        // Get rid of any remaining build nonsense
+        return $this->stripLeftOf($version, '-');
     }
 
     private function stripCustomBuilds(string $version): string
     {
-        $fuckTildes = substr($version, strpos($version, '~'));
-        return substr($version, strpos($version, '+'));
+        $version = $this->stripLeftOf($version, '~');
+        $version = $this->stripLeftOf($version, '+');
+        return $version;
+    }
+
+    private function stripLeftOf(string $subject, string $needle): string
+    {
+
+        return strpos($subject, $needle) === false
+          ? $subject
+          : substr($subject, 0, strpos($subject, $needle));
     }
 }
